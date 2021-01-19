378e8de1d851105f604effd10c9db554327b92eb
LoveAuggie <381724359@qq.com>
2021-01-19 16:41:48 +0800

    敏感词过滤

--- src/Framework/UsefulFunc/Program.cs
+++ src/Framework/UsefulFunc/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Auggie.Lib.Log;
+using Auggie.Lib.Utils;
 
 namespace UsefulFunc
 {
@@ -11,7 +12,11 @@ namespace UsefulFunc
     {
         static void Main(string[] args)
         {
-            CustomLoggerManager.GetCustomLogger("t");
+            //CustomLoggerManager.GetCustomLogger("t");
+
+            SensitiveWordSplit.LoadWords(new List<string>() { "日本", "日出", "日本人" });
+
+            var str = SensitiveWordSplit.Proc("日日本人阿萨德日大厦日本请问日出看见了看见合力科技日日本人");
         }
     }
 }
---
+++ src/Framework/UsefulFunc/Utils/SensitiveWordSplit.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auggie.Lib.Utils
+{
+    public class SensitiveWordSplit
+    {
+        private static Dictionary<char, SwNode> swTree = new Dictionary<char, SwNode>();
+
+        public static string Proc(string article)
+        {
+            int iStart = 0;
+
+            SwNode curNode = null;
+            while (iStart< article.Length)
+            {
+                int iNum = iStart;
+                Dictionary<char, SwNode> CheckTree = swTree;
+
+                CStart:
+                var ic = article[iNum];
+                if (CheckTree.TryGetValue(ic, out curNode))
+                {
+                    if (curNode.isEnd)
+                    {
+                        article = article.Remove(iStart, iNum - iStart + 1);
+                        article = article.Insert(iStart, "***");
+                        iStart = iStart + 3;
+                        continue;
+                    }
+                    else
+                    {
+                        CheckTree = curNode.childs;
+                        iNum++;
+                        goto CStart;
+                    }
+                }
+                else
+                {
+                    iStart++;
+                }
+            }
+            return article;
+        }
+
+        public static void LoadWords(List<string> words)
+        {
+            Dictionary<char, SwNode> tree = swTree;
+            foreach(var word in words)
+            {
+                tree = swTree;
+                SwNode lNode = null;
+                foreach (var wc in word)
+                {
+                    if (tree.TryGetValue(wc, out lNode))
+                    {
+                        tree = lNode.childs;
+                    }
+                    else
+                    {
+                        lNode = new SwNode() { word = wc };
+                        tree[wc] = lNode;
+                        tree = lNode.childs;
+                    }
+                }
+                lNode.isEnd = true;
+            }
+        }
+    }
+
+    public class SwNode
+    {
+        public char word { get; set; }
+
+        public Dictionary<char, SwNode> childs = new Dictionary<char, SwNode>();
+
+        public bool isEnd { get; set; }
+    }
+}
